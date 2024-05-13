61568d1bd06845fcec104fcd78293e5ccd55bc38
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-13 06:38:00 -0700

    improve version check

--- {{ cookiecutter.project_slug }}/main.go
+++ {{ cookiecutter.project_slug }}/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 1 && (os.Args[1] == "-version" || os.Args[1] == "version") {
+	if len(os.Args) > 1 && (os.Args[1] == "-version" || os.Args[1] == "version" || os.Args[1] == "-v") {
 		fmt.Println(version.GetBuildInfo())
 		os.Exit(0)
 	}
